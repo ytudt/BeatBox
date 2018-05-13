305258e39bd14c88edac90349e0d7a1c90800cbc
dongtongtong <dongtongtong@meituan.com>
2018-05-13 11:18:20 +0800

    feat:完成18章-文件读取并显示

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by dt on 2018/5/13.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+    public String getName() {
+        return mName;
+    }
+}
